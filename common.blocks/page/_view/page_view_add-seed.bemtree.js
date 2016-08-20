f91b9d0bbb4ce02e14d47ea07a476ba93abb2e5d
admin <admin@188.160-27.175.8.141.in-addr.arpa>
2016-08-20 15:49:39 +0300

    add blocks

--- common.blocks/page/_view/page_view_add-seed.bemtree.js
+++ common.blocks/page/_view/page_view_add-seed.bemtree.js
@@ -3,35 +3,43 @@
  */
 
 block('page').mod('view', 'add-seed').content()(function() {
-            return {
-                block : 'hello',
-                content : {
-                    tag: 'form',
-                    attrs: {action: '/seed/add', method: 'POST'},
-                    content: [
-                        {
-                            elem: 'greeting',
-                            content: 'Add test message!'
-                        },
-                        {
-                            block: 'input',
-                            mods: {theme: 'islands', size: 'm'},
-                            name: 'name',
-                            placeholder: 'Please enter name'
-                        },
-                        {
-                            block: 'input',
-                            mods: {theme: 'islands', size: 'm'},
-                            name: 'text',
-                            placeholder: 'Please select text'
-                        },
-                        {
-                            block: 'button',
-                            mods: {theme: 'islands', size: 'm', type: 'submit'},
-                            type: 'submit',
-                            text: 'add'
-                        }
-                    ]
+            return [
+                {
+                    block: 'header'
+                },
+                {
+                    block: 'hello',
+                    content: {
+                        tag: 'form',
+                        attrs: {action: '/seed/add', method: 'POST'},
+                        content: [
+                            {
+                                elem: 'greeting',
+                                content: 'Add test message!'
+                            },
+                            {
+                                block: 'input',
+                                mods: {theme: 'islands', size: 'm'},
+                                name: 'name',
+                                placeholder: 'Please enter name'
+                            },
+                            {
+                                block: 'input',
+                                mods: {theme: 'islands', size: 'm'},
+                                name: 'text',
+                                placeholder: 'Please select text'
+                            },
+                            {
+                                block: 'button',
+                                mods: {theme: 'islands', size: 'm', type: 'submit'},
+                                type: 'submit',
+                                text: 'add'
+                            }
+                        ]
+                    }
+                },
+                {
+                    block: 'footer'
                 }
-            }
+            ]
 });
