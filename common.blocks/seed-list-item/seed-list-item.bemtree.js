f33822b6f595dd0d7c505b5dc232dc6945a80e25
Yulia <yuliya.m@localhost>
2016-08-22 15:37:34 +0300

    Seed style

--- common.blocks/seed-list-item/seed-list-item.bemtree.js
+++ common.blocks/seed-list-item/seed-list-item.bemtree.js
@@ -6,46 +6,56 @@ block('seed-list-item').content()(function() {
     var seed = this.ctx.seed;
     return [
         {
-        elem: 'item',
-        content: [
-            {
-                block : 'avatar',
-                img: seed.author_ava,
-                mix: { block : 'seed-list', elem: 'avatar' }
-            },
-            {
-                elem: 'title',
-                content: [
-                    {
-                      elem: 'name',
-                      tag: 'span',
-                      content: seed.author_name
-                    },
-                    {
-                        elem: 'nick',
-                        tag: 'span',
-                        content: '@'+seed.author_nick
-                    },
-                    {
-                        elem: 'nick',
-                        tag: 'span',
-                        content: '- Aug 20'
-                    },
-                ]
+            elem: 'item',
+            content:[
+                {
+                    elem: 'left',
+                    content: {
+                                block : 'avatar',
+                                img: seed.author_ava,
+                                mix: { block : 'seed-list', elem: 'avatar' }
+                    }
+                },
+                {
+                    elem: 'right',
+                    content: [
+                                {
+                                    elem: 'title',
+                                    content: [
+                                        {
+                                          elem: 'name',
+                                          content: seed.author_name
+                                        },
+                                        {
+                                            elem: 'nick',
+                                            content: '@'+seed.author_nick
+                                        },
+                                        {
+                                            elem: 'nick',
+                                            content: 'Aug 20'
+                                        },
+                                    ]
 
-            },
-            {
-                elem: 'msg',
-                content: seed.msg
-            },
-            {
-                block: 'image',
-                url: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR4mLmKpa_q3lgkn9Je2guQG3YMvhRMCQtpyOoSLUOjWSykvfME',
-                alt: 'Seed Image',
-                width: '100%',
-                height: 'auto'
+                                }
+                    ]
+                },
+                {
+                    elem: 'msg',
+                    content: seed.msg
+                },
+                {
+                    elem: 'bottom',
+                    content:{
+                        block: 'image',
+                        url: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR4mLmKpa_q3lgkn9Je2guQG3YMvhRMCQtpyOoSLUOjWSykvfME',
+                        alt: 'Seed Image',
+                        height: 'auto'
+                    }
+                }
 
-            }
-        ]
-}];
+            ]
+        }
+
+    ];
 });
+
