e827b1922b4f4206f3234c21810649aea49e47cc
Elnur Kurtaliev <el92@ya.ru>
2016-08-23 22:25:17 +0300

    Дополнительно дотягиваю инфу о пользователях

--- server/routes.js
+++ server/routes.js
@@ -12,6 +12,7 @@ module.exports = function(conn, passport){
 
     var async = require('async');
     var Seed = require('./models/seed.js');
+    var User = require('./models/user.js');
 
     var seedController = require('./ctl/seedCtrl')(router);
     var userController = require('./ctl/userCtrl')(router);
@@ -100,7 +101,7 @@ module.exports = function(conn, passport){
                 author_name: 'Drew Coleman',
                 author_nick: 'drew_coleman',
                 author_ava: 'http://xage.ru/media/posts/2013/3/25/fotografii-glaz-zhivotnyh-i-nasekomyh.jpg',
-                img:''
+                img:'/img/cat.jpg'
 
             },
             {
@@ -111,15 +112,29 @@ module.exports = function(conn, passport){
                 author_name: 'Steve Nassar',
                 author_nick: 'steve_nassar',
                 author_ava: 'http://www.popmeh.ru/upload/iblock/1d3/1d36d9dd3c9b46f777d0507205cc74b6.jpg',
-                img:''
+                img:'/img/cat.jpg'
             }
         ];
 
         Seed.find(function (err, seedsDb) {
             if (err) return next(err);
             async.each(seedsDb, function(seedDb, callback) {
-                seeds.push(seedDb);
-                callback();
+
+
+                User.findById(seedDb.author, function (err, user) {
+                    if (err) return next(err);
+                    seeds.push({
+                        id: seedDb._id,
+                        msg: seedDb.msg,
+                        datetime: seedDb.datetime,
+                        parent: seedDb.parent[0], //Твит на который сделали ответ
+                        author_name: user.userData.firstName,
+                        author_nick: user.nick,
+                        author_ava: user.avatar,
+                        img:''
+                    });
+                    callback();
+                });
             }, function(err) {
                 if (err) return next(err);
                 render(req, res, {
