17d5c643edb947a5dbab069f85013407028c44a2
Elnur Kurtaliev <el92@ya.ru>
2016-08-27 13:55:33 +0300

    Сохранение сида от текущего юзера

--- common.blocks/addSeed/addSeed.bemhtml.js
+++ common.blocks/addSeed/addSeed.bemhtml.js
@@ -11,20 +11,14 @@ block('addSeed')(
     content()(function() {
         return [
             {
-                elem: 'greeting',
-                content: 'Add test message!'
-            },
-            {
-                block: 'input',
-                mods: {theme: 'islands', size: 'm'},
-                name: 'name',
-                placeholder: 'Please enter name'
-            },
-            {
-                block: 'input',
-                mods: {theme: 'islands', size: 'm'},
+                block: 'textarea',
+                mods: {theme: 'islands', size: 'm', width : 'available'},
                 name: 'text',
-                placeholder: 'Please select text'
+                attrs: {
+                    maxlength: 140,
+                    required: true
+                },
+                placeholder: 'Введите сообщение'
             },
             {
                 block: 'button',
